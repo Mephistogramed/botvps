56925837e8057eefc0d516dbfb2dedeea8feafb3
Peble Jackson <65134846+Mephistogramed@users.noreply.github.com>
2022-12-09 19:01:04 +0530

    Update Forsaken.py

--- Forsaken.py
+++ Forsaken.py
@@ -4,12 +4,12 @@ import discord
 from discord.ext import commands
 import os
 
-client = commands.Bot(command_prefix= '_', intents = discord.Intents.all())
+client = commands.Bot(command_prefix= '-', intents = discord.Intents.all())
 
 client.remove_command('help')
 # Defining the load cogs(commands) function, this pulls all the commands in the command folder.
 async def load():
-    for filename in os.listdir(r'./APF-Python-main/commands'):
+    for filename in os.listdir('/root/botvps/commands'):
         if filename.endswith('.py'):
             await client.load_extension(f'commands.{filename[:-3]}')
             print(f'Loaded {filename}')
@@ -24,7 +24,7 @@ async def on_ready():
 async def main():
     async with client:
         await load()
-        await client.start("ODA3MzQ3MTQ2OTU3MjU4NzUz.GbaMVE.qQQ4hQdCn8cP98Cn2UBiT2VR_DiE_QT0okMbxQ")
+        await client.start("")
 
 # This connects the script with the bot application.
 
